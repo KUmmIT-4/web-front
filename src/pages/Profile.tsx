dc95cca21a85fbaec43592f658b8b421028234e0
TakioN <jungbeen0724@gmail.com>
2025-06-28 22:19:15 +0900

    feat: update profile page UI

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,20 +1,25 @@
+import Header from "@/components/header";
+
 const Profile = () => {
   return (
     <div className="max-w-[540px] mx-auto">
-      <header className="font-bold text-xl mt-4">내 정보</header>
+      <Header noLogo={true} title={"내정보"} />
       <main className="mt-4 max-w-[540px] w-full">
-        <div className="px-4 py-5">
+        <div className="px-9 pt-5 pb-11 flex gap-8 border-b border-[var(--disabled)]">
           {/* <img /> */}
-          <div className="size-20 bg-[var(--disabled)] rounded-[50%] mb-5"></div>
-          <p className="text-start font-bold underline">example.gmail.com</p>
+          <div className="size-25 bg-[var(--disabled)] rounded-[50%] mb-5"></div>
+          <div className="flex flex-col py-4 gap-2 text-start">
+            <span className="font-bold">harucoding 님</span>
+            <span className="font-bold">1000점</span>
+          </div>
         </div>
-        <div className=" border-t h-14 text-start">
-          <span className="ml-4 text-xl font-medium h-full block leading-14">
-            마이페이지
+        <div className=" border-b border-[var(--disabled)] h-14 text-start">
+          <span className="ml-4 font-medium h-full block leading-14">
+            내정보 수정
           </span>
         </div>
-        <div className=" border-t h-14 text-start">
-          <span className="ml-4 text-xl font-medium h-full block leading-14">
+        <div className=" border-b border-[var(--disabled)] h-14 text-start">
+          <span className="ml-4 font-medium h-full block leading-14">
             로그아웃
           </span>
         </div>
