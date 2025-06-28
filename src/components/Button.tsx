dec120c403d7447b98197693e9cba0acd84f5184
Gahyun Kim <sally_0113@naver.com>
2025-06-28 17:51:19 +0900

    intro, log, reg

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 
 interface ButtonProps {
+  className?: string;
   icon: React.ReactNode;
   label: string;
   onClick?: () => void;
   //   color?: string;
 }
 
-const Button = ({ icon, label, onClick }: ButtonProps) => {
+const Button = ({ className, icon, label, onClick }: ButtonProps) => {
   return (
-    <button onClick={onClick}>
+    <button
+      className={`w-96 h-16 rounded-full font-bold text-[18px] py-2 px-4 cursor-pointer
+        ${className}`}
+      onClick={onClick}
+    >
       <span>{icon}</span>
       <span>{label}</span>
     </button>
--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -1,7 +1,26 @@
 // import React from "React";
+import Button from "@/components/Button";
 
 const Introduction = () => {
-  return <h1>Introduction page</h1>;
+  return (
+    <div className="flex flex-col gap-64">
+      <div className="flex flex-col items-center pt-36">
+        <h1 className="text-blue-500 text-5xl font-bold ">하루코딩</h1>
+        <div className="text-gray-600 text-[17px] font-bold pt-6">
+          <div>하루 한 문제, 코딩이 습관이</div>
+          <div>되는 순간까지</div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-4">
+        <div>
+          <Button className="bg-blue-200" icon="" label="로그인" />
+        </div>
+        <div>
+          <Button className="bg-blue-200" icon="" label="회원가입" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Introduction;
