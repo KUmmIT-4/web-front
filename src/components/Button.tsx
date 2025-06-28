669f252c971577849ae5b9aa75fbdc627e1b2c13
Gahyun Kim <sally_0113@naver.com>
2025-06-29 02:12:52 +0900

    add: 로그인 막기 기능 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,15 +4,19 @@ interface ButtonProps {
   className?: string;
   icon?: React.ReactNode;
   label: string;
+  // onClick?: () => any;
   onClick?: () => void;
+  // onClick?: React.ChangeEventHandler<HTMLInputElement>;
+  disabled?: boolean;
 }
 
-const Button = ({ className, icon, label, onClick }: ButtonProps) => {
+const Button = ({ className, icon, label, onClick, disabled }: ButtonProps) => {
   return (
     <button
       className={`w-96 h-16 rounded-full font-bold text-[18px] py-2 px-4 cursor-pointer
         ${className}`}
       onClick={onClick}
+      disabled={disabled}
     >
       <span>{icon}</span>
       <span>{label}</span>
