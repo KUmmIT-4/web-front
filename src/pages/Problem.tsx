b0647f9f5abe7cd010cc88fb05ea1a8b93808559
한성준 <june123000@naver.com>
2025-06-28 18:54:33 +0900

    feat: problem page 만드는중

--- src/pages/Problem.tsx
+++ src/pages/Problem.tsx
@@ -1,7 +1,29 @@
-// import React from 'react'
+import React from 'react'
+import { ChevronLeft, User } from "lucide-react"; // chevron-left import
+import { Link } from 'react-router-dom';
 
 const Problem = () => {
-  return <h1>Problem</h1>;
+  return (
+    <>
+      {/* navbar */}
+      <div className="flex justify-between">
+        {/* 뒤로가기 */}
+        <Link to='' className='p-3 w-12'>
+          <ChevronLeft />
+        </Link>
+        {/* 프로필 버튼 */}
+        <Link to='' className='p-3 w-12'>
+          <User />
+        </Link>
+      </div>
+
+      {/* 문제 설명 */}
+
+      {/* 코드 */}
+
+      {/* 선택지 */}
+    </>
+  );
 };
 
 export default Problem;
