2a517266957e1246f033ead7854ed3fd2bcc70b2
TakioN <jungbeen0724@gmail.com>
2025-06-29 04:15:19 +0900

    feat: add back-arrow func in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 import user from "@/assets/images/user.png";
 import backArrow from "@/assets/images/back-arrow.png";
 
 interface HeaderProps {
   noLogo?: boolean;
   title?: string;
+  gotoHome?: () => void;
 }
 
-const renderHeaderValue = (noLogo: boolean, title: string) => {
+const renderHeaderValue = ({ noLogo, title, gotoHome }: HeaderProps) => {
   if (noLogo) {
     return (
       <header className="h-12 flex px-5 w-full items-center justify-center relative">
         <div className="flex gap-7 absolute left-5">
-          <img className="w-2.5 h-4.5" src={backArrow} />
+          <img className="w-2.5 h-4.5" src={backArrow} onClick={gotoHome} />
         </div>
         <span className="text-black text-2xl font-bold">{title}</span>
       </header>
@@ -32,16 +35,12 @@ const renderHeaderValue = (noLogo: boolean, title: string) => {
 };
 
 function Header({ noLogo = false, title = "title" }: HeaderProps) {
-  return (
-    <>
-      {/* <span className="text-[var(--primary)] text-2xl font-bold">하루코딩</span>
+  const navigate = useNavigate();
 
-      <div className="flex gap-7">
-        <img className="size-5" src={user} />
-      </div> */}
-      {renderHeaderValue(noLogo, title)}
-    </>
-  );
+  const gotoHome = () => {
+    navigate("/home");
+  };
+  return <>{renderHeaderValue({ noLogo, title, gotoHome })}</>;
 }
 
 export default Header;
